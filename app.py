91b72f745fc65bb3e7f3907eb554256f8223adde
yasin taha erol <yasintahaerol@gmail.com>
2021-07-14 11:46:53 +0300

    app.py updated

--- app.py
+++ app.py
@@ -1,25 +1,20 @@
 from flask import Flask, jsonify
 import json
-import os
-import configparser
+
 app = Flask(__name__)
 
-path = 'config-vol/conf.json/conf.json'
-
-
-with open(path, 'rb') as f:
-  config = json.load(f)
 
 @app.route('/')
 def hello():
-    print(config)
+    with open('config/conf.json', 'rb') as f:
+        config = json.load(f)
+        f.close()
     return jsonify({
-        'Hi':"Hello World!"
+        'body': config
     })
 
 if __name__ == "__main__":
-  app.debug = True
-  app.run()
-  app.run(host='0.0.0.0', port=5000)
-
+    app.debug = True
+    app.run()
+    app.run(host='0.0.0.0', port=5000)
 
